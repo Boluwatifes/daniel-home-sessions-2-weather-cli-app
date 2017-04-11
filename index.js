29c6403d429203dcdebb64013a3dd1b97315488b
Bamidele Daniel <greatbolutife@gmail.com>
2017-04-11 14:16:28 +0100

    Add runtime functions

---
+++ index.js
@@ -0,0 +1,14 @@
+'use strict'
+
+let lib = require('./lib/lib.js');
+let chalk = require('chalk');
+let inquirer = require('inquirer');
+let figlet = require('figlet');
+
+
+console.log(
+  chalk.black.bgWhite(
+    figlet.textSync('Simple Weather', { horizontalLayout: 'default' })
+  )
+);
+
